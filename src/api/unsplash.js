22c64f9f098f0c72ee03aa4860f8e571490be18c
Furkan Aktepe <85746370+furkanaktepe01@users.noreply.github.com>
2021-09-27 16:51:18 +0300

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.unsplash.com",
     headers: {
-        Authorization: "Client-ID wY6hQU31hqjEhImflmiUiFXWAeXqbZKc3CZOZbzMh0c"
+        Authorization: "Client-ID wY6hQU*************************bzMh0c"
     }
 });
 
+// API key is hidden for security purposes.
